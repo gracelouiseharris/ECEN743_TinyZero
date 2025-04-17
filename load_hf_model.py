58a4e0df3a59bff7e0a6f7cea2ef572c523b274f
Rishabh Tripathi <trishabh1999@login4.cluster>
2025-04-17 11:56:23 -0500

    added Qwen3B

--- load_hf_model.py
+++ load_hf_model.py
@@ -3,7 +3,7 @@ from huggingface_hub import snapshot_download
 
 # Use current working directory as base
 base_path = os.getcwd()
-model_subdir = "models/qwen2.5-0.5b"
+model_subdir = "models/qwen2.5-3b"
 LOCAL_MODEL_DIR = os.path.join(base_path, model_subdir)
 
 # Create the directory if it doesn't exist
@@ -11,7 +11,7 @@ os.makedirs(LOCAL_MODEL_DIR, exist_ok=True)
 
 # Download the model into the local directory
 snapshot_download(
-    repo_id="Qwen/Qwen2.5-0.5B",
+    repo_id="Qwen/Qwen2.5-3B",
     cache_dir=LOCAL_MODEL_DIR,  # Optional: cache metadata here
     local_dir=LOCAL_MODEL_DIR,  # Actual directory to store model
     local_dir_use_symlinks=False,
